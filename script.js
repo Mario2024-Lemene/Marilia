8d1526c1d39273628c2e9bd2a158c0e56cfca1d2
Mario2024-Lemene <177216495+Mario2024-Lemene@users.noreply.github.com>
2024-08-31 22:03:39 +0200

    Escolhendo nova paleta de cores

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const resp = document.querySelectorAll("h1")[0]
+const erro = document.querySelector("h2")
 let num = 1
 do{
 const nomeInicial = prompt("Deguite o seu nome: ")
@@ -7,6 +8,8 @@ if(nome == "MARTA"){
     alert("Nunca te chamo de Marta, tens que notar isso")
 }else if(nome == "MARILIA"){
     alert("Estavas mesmo proxima de chegar no objectivo")
+    erro.innerText = "Me amas"
+    break
 }else if(nome == "SUBMISSA"){
     alert("Acertou na resposta, minha Marilia Favorita")
     resp.innerText = "Te amo"
