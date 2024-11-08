2a58adf6671d282701f693872c5fa4d004dcc61d
Mario2024-Lemene <177216495+Mario2024-Lemene@users.noreply.github.com>
2024-11-09 00:12:08 +0200

    alterando projecto Marilia

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 const resp = document.querySelectorAll("h1")[0]
-const erro = document.querySelector("h2")
-let num = 1
-do{
-const nomeInicial = prompt("Deguite o seu nome: ")
-const nome = nomeInicial.toUpperCase().trim()
-if(nome == "MARTA"){
-    alert("Nunca te chamo de Marta, tens que notar isso")
-}else if(nome == "MARILIA"){
-    alert("Estavas mesmo proxima de chegar no objectivo")
-    erro.innerText = "Me amas"
-    break
-}else if(nome == "SUBMISSA"){
-    alert("Acertou na resposta, minha Marilia Favorita")
-    resp.innerText = "Te amo"
-    break
-}else if(nome == "PALHACO" || nome == "PALHAÇO"){
-    alert("Comediante mesmo, mas não é esse")
-}else{
-    alert("Falhaste no teste, ate parece que não me coneheçes")
-}
-}while(num = 1)
+do {
+    const nomeInicial = prompt("Deguite o seu nome: ")
+    const nome = (nomeInicial.toUpperCase()).trim()
+    if (nome == "MARTA") {
+        alert("Nunca te chamo de Marta, tens que notar isso")
+     } else if (nome == "SUBMISSA") {
+        alert("Acertou na resposta, minha Marilia Favorita")
+        resp.innerText = "Te amo"
+        break
+    } else if (nome == "PALHACO" || nome == "PALHAÇO") {
+        alert("Comediante mesmo, mas não é esse")
+    } else {
+        alert("Falhaste no teste, ate parece que não me coneheçes")
+    }
+} while (true)
